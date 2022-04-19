f5ee1df535d5358ced949db862dbeb628a721781
Kirill_Eselev <Kirill_Eselev@epam.com>
2022-04-20 03:44:19 +0400

    initialize FE part of app, update .gitignore file

---
+++ FE/src/App.tsx
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="logo" width={300} />
+    </div>
+  );
+}
+
+export default App;
