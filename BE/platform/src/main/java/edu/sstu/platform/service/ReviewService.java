1d7190ca8ebb7070758a5103aa49260df2425b05
Semyon Afanasyev <beforsss@yandex.ru>
2022-05-27 01:39:32 +0400

    add critic marker to review

--- BE/platform/src/main/java/edu/sstu/platform/service/ReviewService.java
+++ BE/platform/src/main/java/edu/sstu/platform/service/ReviewService.java
@@ -3,14 +3,12 @@ package edu.sstu.platform.service;
 import edu.sstu.platform.dto.request.MovieReviewRequestDto;
 import edu.sstu.platform.dto.response.ReviewResponseDto;
 import edu.sstu.platform.mapper.ReviewMapper;
-import edu.sstu.platform.model.QReview;
 import edu.sstu.platform.repo.ReviewRepo;
 import java.util.UUID;
 import javax.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,7 +20,6 @@ public class ReviewService {
 
   private final ReviewRepo reviewRepo;
   private final ReviewMapper reviewMapper;
-  private final QReview qReview = QReview.review;
 
   public UUID createReview(MovieReviewRequestDto movieReviewRequestDto, UUID movieId, UUID userId) {
     var review = reviewMapper.toEntity(movieReviewRequestDto, movieId, userId);
@@ -50,8 +47,7 @@ public class ReviewService {
 
   @Transactional(readOnly = true)
   public Page<ReviewResponseDto> findReviews(UUID movieId, Pageable pageable) {
-    var unsortedPageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize());
-    var reviewIdPage = reviewRepo.findReviewIdsByMovieId(movieId, unsortedPageable);
+    var reviewIdPage = reviewRepo.findReviewIdsByMovieId(movieId, pageable);
     var reviewIds = reviewIdPage.getContent();
     var reviews = reviewRepo.findByIdIn(reviewIds, pageable.getSort());
     var ReviewDtoList = reviewMapper.toDto(reviews);
